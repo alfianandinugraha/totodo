8e924d8ef00c317cadff717266b1d9bac6aadf74
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-03-10 20:04:52 +0700

    feat(components): adding TodoCard component

--- src/components/TodoCard.tsx
+++ src/components/TodoCard.tsx
@@ -1,3 +1,4 @@
+import { Button, ButtonGroup, makeStyles, Typography } from '@material-ui/core'
 import React, { ReactElement } from 'react'
 
 export interface Todo {
@@ -10,6 +11,46 @@ interface TodoCardProps {
   payload: Todo
 }
 
+const useStyles = makeStyles(({ spacing, breakpoints }) => ({
+  root: {
+    padding: spacing(2),
+    border: '0.5px solid #BBBBBB',
+    display: 'flex',
+    flexDirection: 'column',
+    marginBottom: spacing(2),
+    borderRadius: '5px',
+    [breakpoints.up('sm')]: {
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      flexDirection: 'row',
+      '& > p': {
+        marginBottom: '0px',
+      },
+    },
+    '& > p': {
+      marginBottom: spacing(1),
+    },
+  },
+}))
+
 export default function TodoCard({ payload }: TodoCardProps): ReactElement {
-  return <div />
+  const classes = useStyles()
+  return (
+    <div className={classes.root}>
+      <Typography>{payload.description}</Typography>
+      <div>
+        <ButtonGroup>
+          <Button variant="outlined" color="primary">
+            Finish
+          </Button>
+          <Button variant="outlined" color="primary">
+            Update
+          </Button>
+          <Button variant="outlined" color="secondary">
+            Delete
+          </Button>
+        </ButtonGroup>
+      </div>
+    </div>
+  )
 }
