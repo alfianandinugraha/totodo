808490cd027292a32897a43e69c5a55c3b71ca46
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-03-12 01:20:04 +0700

    feat(pages/profile): adding ui form

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,12 +1,37 @@
 import HeaderDashboard from '@/components/HeaderDashboard'
-import { Container, Typography } from '@material-ui/core'
+import {
+  Container,
+  TextField,
+  Typography,
+  Button,
+  makeStyles,
+} from '@material-ui/core'
 import React, { ReactElement } from 'react'
 
+const useStyles = makeStyles(({ spacing }) => ({
+  formUpdate: {
+    marginTop: spacing(3),
+    display: 'flex',
+    flexDirection: 'column',
+    '& > button': {
+      marginTop: spacing(3),
+    },
+  },
+}))
+
 export default function ProfilePage(): ReactElement {
+  const classes = useStyles()
+
   return (
     <Container maxWidth="sm">
       <HeaderDashboard />
       <Typography>Update Profile</Typography>
+      <form className={classes.formUpdate}>
+        <TextField label="Fullname" />
+        <Button variant="contained" color="primary">
+          Update
+        </Button>
+      </form>
     </Container>
   )
 }
