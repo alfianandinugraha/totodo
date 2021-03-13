6b1efdba79a0047c8ad7a36a878c3abb8dd59169
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-03-13 19:57:02 +0700

    feat(utils/firebase): adding default value for collections

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -11,6 +11,10 @@ const firebaseConfig = {
   appId: '1:50350283624:web:4cb47ed8df8ce77ea1d4dd',
 }
 
+const USERS_COLLECTION = 'users'
+const TODOS_COLLECTION = 'todos'
+
 if (!firebase.apps.length) firebase.initializeApp(firebaseConfig)
 
+export { USERS_COLLECTION, TODOS_COLLECTION }
 export default firebase
